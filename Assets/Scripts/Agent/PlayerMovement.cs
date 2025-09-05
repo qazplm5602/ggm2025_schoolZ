3387276ece9e1193fb9a120198fd95d063fe4ee4
domi <domi@domi.kr>
2025-09-05 11:20:58 +0900

    플레이어 애니메이션 추가

--- Assets/Scripts/Agent/PlayerMovement.cs
+++ Assets/Scripts/Agent/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : AgentMovement
     [SerializeField] private Transform camTrm;
     [SerializeField] private float camHeight;
     private CharacterController controller;
+    private Animator animator;
     public float speed = 5f;
     public float jumpPower = 5f;
     public float camRotSpeed = 15f;
@@ -17,6 +18,7 @@ public class PlayerMovement : AgentMovement
     {
         base.InitAgent(agent);
         controller = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
         input.OnJumpChange += HandleChangeJump;
     }
 
@@ -27,8 +29,10 @@ public class PlayerMovement : AgentMovement
         // 예시: WASD 입력으로 이동 (InputSO에 맞게 수정 필요)
         Vector2 moveDir = input.GetMoveDir();
         Vector3 move = new Vector3(moveDir.x, 0, moveDir.y);
-        // move = transform.TransformDirection(move);
-        move = camTrm.TransformDirection(move);
+
+        // 카메라의 y축만 적용해서 이동 방향 변환
+        Quaternion camYRot = Quaternion.Euler(0, camTrm.eulerAngles.y, 0);
+        move = camYRot * move;
         move *= speed;
 
         // if (move.sqrMagnitude > 0f)
@@ -50,6 +54,7 @@ public class PlayerMovement : AgentMovement
             }
         }
         camTrm.position = transform.position + Vector3.up * camHeight;
+        animator.SetFloat("MoveSpeed", Mathf.Lerp(animator.GetFloat("MoveSpeed"), move.sqrMagnitude > 0f ? 1 : 0, Time.deltaTime * 10));
 
         // 중력 적용
         if (controller.isGrounded)
