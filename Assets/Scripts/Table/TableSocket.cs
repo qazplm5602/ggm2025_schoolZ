7bd5002762ee64bd22a45cb8a53a78631c0b2adb
domi <domi@domi.kr>
2025-09-08 14:47:13 +0900

    테이블 감지

--- Assets/Scripts/Table/TableSocket.cs
+++ Assets/Scripts/Table/TableSocket.cs
@@ -3,4 +3,45 @@ using UnityEngine;
 
 public class TableSocket : MonoBehaviour
 {
+    [SerializeField] private InputSO input;
+    private TableCore currentTable;
+    private Collider[] overlapResult = new Collider[1];
+    [SerializeField] private LayerMask detectLayer;
+    [SerializeField] private Transform detectPoint;
+    [SerializeField] private Vector3 detectSize;
+
+    void OnEnable()
+    {
+        input.OnInteractPress += HandleInteract;
+    }
+
+    void OnDisable()
+    {
+        input.OnInteractPress -= HandleInteract;
+    }
+
+    public void AttachNearTable()
+    {
+        int count = Physics.OverlapBoxNonAlloc(detectPoint.position, detectSize, overlapResult, detectPoint.rotation, detectLayer);
+        print($"count: {count}");
+    }
+
+    private void HandleInteract()
+    {
+        if (currentTable)
+        {
+
+        }
+        else
+        {
+            AttachNearTable();
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(detectPoint.position, detectSize);
+        Gizmos.color = Color.white;
+    }
 }
